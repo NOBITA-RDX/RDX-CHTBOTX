2a9b18b03b68f05fb0898770d004221646f77e38
NOBITA-RDX <141653116+NOBITA-RDX@users.noreply.github.com>
2023-09-10 16:16:35 +0530

    Update start.py

--- Mickey/modules/start.py
+++ Mickey/modules/start.py
@@ -22,39 +22,3 @@ from Mickey.modules.helpers import (
     SOURCE_READ,
     START,
 )
-
-
-@MickeyBot.on_cmd(["start", "aistart"])
-
-
-@MickeyBot.on_cmd("help")
-async def help(client: MickeyBot, m: Message):
-    if m.chat.type == ChatType.PRIVATE:
-        hmm = await m.reply_photo(
-            photo=random.choice(IMG),
-            caption=HELP_READ,
-            reply_markup=InlineKeyboardMarkup(HELP_BTN),
-        )
-        await add_served_user(m.from_user.id)
-    else:
-        await m.reply_photo(
-            photo=random.choice(IMG),
-            caption="**ʜᴇʏ, ᴘᴍ ᴍᴇ ғᴏʀ ʜᴇʟᴘ ᴄᴏᴍᴍᴀɴᴅs!**",
-            reply_markup=InlineKeyboardMarkup(HELP_BUTN),
-        )
-        await add_served_chat(m.chat.id)
-
-
-@MickeyBot.on_cmd("repo")
-async def repo(_, m: Message):
-    await m.reply_text(
-        text=SOURCE_READ,
-        reply_markup=InlineKeyboardMarkup(CLOSE_BTN),
-        disable_web_page_preview=True,
-    )
-
-
-@MickeyBot.on_message(filters.new_chat_members)
-async def welcome(_, m: Message):
-    for member in m.new_chat_members:
-        await m.reply_photo(photo=random.choice(IMG), caption=START)
